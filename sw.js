0532e27293d272d1fa4f9fc19c6acf3fa14945d4
SamiSteyre <111428125+SamiSteyre@users.noreply.github.com>
2026-05-07 18:49:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = 'fitbuddy-v7';
+const CACHE_NAME = 'fitbuddy-v10';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/fitbuddy/',
+  '/fitbuddy/index.html',
+  '/fitbuddy/manifest.json',
+  '/fitbuddy/icon-192.png',
+  '/fitbuddy/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME && caches.delete(k)))));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
