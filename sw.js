7a62c8eec6834ddf45d107b0e53c3ab642c1ce7a
SamiSteyre <111428125+SamiSteyre@users.noreply.github.com>
2026-05-07 18:38:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
-self.addEventListener('fetch', function(event) {
-    // Ce code permet juste à l'app d'être considérée comme "installable"
+const CACHE_NAME = 'fitbuddy-v7';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
