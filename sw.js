b99d64321dce2c6e90dbe3c7e984fb9e7d05e75a
SamiSteyre <111428125+SamiSteyre@users.noreply.github.com>
2026-05-07 19:02:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', function(event) {
-    // Ce code permet juste à l'app d'être considérée comme "installable"
+const CACHE_NAME = 'fitbuddy-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icone.png'
+];
+
+// Installation : Mise en cache des fichiers de base
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
